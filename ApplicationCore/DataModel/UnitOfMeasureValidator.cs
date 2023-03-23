32dc042625dbba9e950a086d605673708833780d
adamtlee <adam.lee346@gmail.com>
2023-03-22 18:56:48 -0600

    completed crud operation

---
+++ ApplicationCore/DataModel/UnitOfMeasureValidator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using FluentValidation;
+
+namespace ApplicationCore.DataModel
+{
+	public class UnitOfMeasureValidator : AbstractValidator<UnitOfMeasure>
+	{
+		public UnitOfMeasureValidator()
+		{
+			RuleFor(unitOfMeasure => unitOfMeasure.Description)
+				.NotEmpty();
+			RuleFor(unitOfMeasure => unitOfMeasure.ExternalId)
+				.MaximumLength(4);
+		}
+	}
+}
+
---
+++ ApplicationCore/Services/UnitsOfMeasure/IUnitOfMeasureDbAccess.cs
@@ -0,0 +1,12 @@
+﻿using ApplicationCore.DataModel;
+
+namespace ApplicationCore.Services; 
+
+public interface IUnitOfMeasureDbAccess
+{
+    Task<List<UnitOfMeasure>> GetAllUnitsOfMeasureAsync();
+    Task<UnitOfMeasure> CreateUnitOfMeasure(UnitOfMeasure uom);
+    Task<UnitOfMeasure> UpdateUnitOfMeasure(UnitOfMeasure uom);
+    Task<int> DeleteUnitsOfMeasure(HashSet<long> uomIds);
+
+}
