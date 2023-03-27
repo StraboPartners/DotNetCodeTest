3585a2ed7948a22b8c50c25728bb0c754d31a8a2
Andrew-Dron27 <Andrew.C.Dron@protonmail.com>
2023-03-26 23:13:23 -0600

    Add database migration

--- ApplicationCore/Migrations/20230327045734_IncreaseExternalIdSize.cs
+++ ApplicationCore/Migrations/20230327045734_IncreaseExternalIdSize.cs
@@ -4,19 +4,37 @@
 
 namespace ApplicationCore.Migrations
 {
-    /// <inheritdoc />
-    public partial class IncreaseExternalIdSize : Migration
-    {
-        /// <inheritdoc />
-        protected override void Up(MigrationBuilder migrationBuilder)
-        {
+	/// <inheritdoc />
+	public partial class IncreaseExternalIdSize : Migration
+	{
+		/// <inheritdoc />
+		protected override void Up(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterColumn<string>(
+				name: "ExternalId",
+				table: "PriceTypes",
+				type: "character varying(7)",
+				maxLength: 7,
+				nullable: true,
+				oldClrType: typeof(string),
+				oldType: "character varying(4)",
+				oldMaxLength: 4,
+				oldNullable: true);
+		}
 
-        }
-
-        /// <inheritdoc />
-        protected override void Down(MigrationBuilder migrationBuilder)
-        {
-
-        }
-    }
+		/// <inheritdoc />
+		protected override void Down(MigrationBuilder migrationBuilder)
+		{
+			migrationBuilder.AlterColumn<string>(
+				name: "ExternalId",
+				table: "PriceTypes",
+				type: "character varying(4)",
+				maxLength: 4,
+				nullable: true,
+				oldClrType: typeof(string),
+				oldType: "character varying(7)",
+				oldMaxLength: 7,
+				oldNullable: true);
+		}
+	}
 }
